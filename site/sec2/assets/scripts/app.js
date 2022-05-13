f76d2be3f0209149d9c92d5ebf107c79e252b4ad
jeandrei <jeandreiwalter@gmail.com>
2022-05-13 19:27:30 +0000

    aula 39

---
+++ site/sec2/assets/scripts/app.js
@@ -0,0 +1,47 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator,resultBeforeCalc,CalcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${CalcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add(){  
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+',initialResult,enteredNumber);
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput(); 
+    const initialResult = currentResult;   
+    currentResult = currentResult - parseInt(enteredNumber);
+    createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput(); 
+    const initialResult = currentResult;   
+    currentResult = currentResult * parseInt(enteredNumber);
+    createAndWriteOutput('*',initialResult,enteredNumber);
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput(); 
+    const initialResult = currentResult;   
+    currentResult = currentResult / parseInt(enteredNumber);
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
