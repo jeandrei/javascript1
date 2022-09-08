9cd0c2f1040e8b5ff58066e61597ce7d86355a45
jeandrei <jeandreiwalter@gmail.com>
2022-09-08 18:05:05 +0000

    atualizacao

---
+++ site/MEUS/ModeloGetDateAsyncAwait/app.js
@@ -0,0 +1,62 @@
+/**
+ * 1 Função que recebe uma url faz um fech e retorna uma nova promise
+ */
+function requestData(url){
+  return new Promise((resolve, reject)=>{
+   fetch(url)
+   // pega os dados retornados e transforma em json
+   .then(res => res.json())
+   // se der tudo certo resolve/retorna os dados
+   .then(data => resolve(data))
+   // caso aconteça algum erro rejeita e retorna o erro
+   .catch(err => reject(err));
+  })
+
+}
+
+/**
+ * 
+ * Função que pega os dados através da função requestData
+ * cria um html e retorna o código html
+ * 
+ */
+async function getData(){
+  let output = '';
+  try{
+      // pega os dados da API e passa para a const user observe o await
+      // com o await aguarda até que os dados cheguem para dar continuidade no código
+      const users = await requestData('http://localhost:3000/users');    
+      // para cada usuário dentro da const acumulamos no html
+      users.forEach(function(user){
+      output += `<li>Id:${user.id}, Name:${user.name}, Second Name: ${user.secondName}, Email: ${user.email}</li>`;
+    });
+  
+  } catch (err){
+    output += 'URL inválida';
+  }
+  // retorna o html pronto para ser inserido na página
+  return output;
+}
+
+
+async function showData(){
+  // Chama a função que pega os dados
+  const html = await getData();
+  //insere no body
+  document.querySelector('body').innerHTML = html;  
+}
+
+const App = (function(){
+
+  return {
+    // cria a função que inicia o APP
+    init: function(){
+      // Chama a função que coloca o html na página
+      showData();
+    }
+  }
+})();
+
+
+// Inicia o APP
+App.init();
