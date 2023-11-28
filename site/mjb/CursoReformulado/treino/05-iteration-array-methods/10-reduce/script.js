435e36ba9b7e757b5ded298b7490d8f98f8686b7
jeandrei <jeandreiwalter@gmail.com>
2023-11-28 17:49:20 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/05-iteration-array-methods/10-reduce/script.js
+++ site/mjb/CursoReformulado/treino/05-iteration-array-methods/10-reduce/script.js
@@ -1,20 +1,18 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Create a const array numbers from 1 to 10
+
+/* reduce works like this array.reduce(function(accumulator, currentValue){
+    return whatever you want
+}, 0) where zero is the initial value to accumulator */
+
 
 // Add all numbers together
-const sum = numbers.reduce(function (accumulator, currentValue) {
-  return accumulator + currentValue;
-}, 0);
+/* Create a const sum and using reduce sum all numbers inside array numbers then console sum*/
 
-const sum2 = numbers.reduce((acc, cur) => acc + cur, 0);
+/* Now create a const num2 and sum all numbers in the array numbers but this time using short version then console sum2*/
 
 // Using a for loop
-const sum3 = () => {
-  let acc = 0;
-  for (const cur of numbers) {
-    acc += cur;
-  }
-  return acc;
-};
+/* Create a const sum3 and sum all numbers inside array numbers but this time use for loop */
+
 
 // Shopping cart example (objects)
 const cart = [
@@ -23,8 +21,6 @@ const cart = [
   { id: 3, name: 'Product 3', price: 200 },
 ];
 
-const total = cart.reduce(function (acc, product) {
-  return acc + product.price;
-}, 0);
 
-console.log(total);
+/* Create a const total and using reduce get the total of all products inside array cart then console total */
+
---
+++ site/mjb/CursoReformulado/treino/05-iteration-array-methods/10-reduce/todo.js
@@ -0,0 +1,26 @@
+// Create a const array numbers from 1 to 10
+
+/* reduce works like this array.reduce(function(accumulator, currentValue){
+    return whatever you want
+}, 0) where zero is the initial value to accumulator */
+
+
+// Add all numbers together
+/* Create a const sum and using reduce sum all numbers inside array numbers then console sum*/
+
+/* Now create a const num2 and sum all numbers in the array numbers but this time using short version then console sum2*/
+
+// Using a for loop
+/* Create a const sum3 and sum all numbers inside array numbers but this time use for loop */
+
+
+// Shopping cart example (objects)
+const cart = [
+  { id: 1, name: 'Product 1', price: 130 },
+  { id: 2, name: 'Product 2', price: 150 },
+  { id: 3, name: 'Product 3', price: 200 },
+];
+
+
+/* Create a const total and using reduce get the total of all products inside array cart then console total */
+
