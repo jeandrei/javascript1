5bdecbdfbf0c9b286909d20df62d798267313ce2
jeandrei <jeandreiwalter@gmail.com>
2023-11-29 01:43:57 +0000

    atualizacao

---
+++ site/mjb/CursoReformulado/treino/06-document-object-model/01-dom-intro/todo.js
@@ -0,0 +1,19 @@
+// Global window object
+// Log the window object
+
+// The document object is part of the window object
+// Log the document object pay attention, use console .dir to see the attributes console.log will just show the code
+
+// We can access DOM elements directly with properties
+// Log the body element
+
+// Log the link zero from document
+
+// We can set properties of elements
+// Set the body to <h1>Hello from body</h1>
+
+// We also have methods to select elements more directly
+// select the element by id main an then change the html to <h1>Hello from main</h1>
+
+// We can use selectors as css, with querySelector select the id main and h1 and change the text to hello
+
