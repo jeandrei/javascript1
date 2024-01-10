8cd1c4527dc747f56419ef360012e5f4821333ab
jeandrei <jeandreiwalter@gmail.com>
2024-01-10 01:11:24 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/todo.js
+++ site/mjb/CursoReformulado/treino/06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/todo.js
@@ -1,44 +1,38 @@
 // querySelectorAll()
 // Returns a NodeList
 
-const listItems = document.querySelectorAll('.item');
+// Create a const listItems with all elements with class item use querySelectorAll
 
 // Access elements by index
-console.log(listItems[1].innerText);
+// Console log listItems index 1 innerText
 
 // Setting a color for specific element
-listItems[1].style.color = 'red';
+// Set the listItems index 1 color to red
 
 // We can use forEach() on a NodeList
-listItems.forEach((item, index) => {
-  item.style.color = 'red';
-
-  if (index === 1) {
-    item.remove();
-  }
-
-  if (index === 0) {
-    item.innerHTML = ` Oranges
+/* Using forEach go through listItems array and change the color to red and 
+if the index = 1 then remove the item  if the index = 0 then add the html below
+Oranges
     <button class="remove-item btn-link text-red">
       <i class="fa-solid fa-xmark"></i>
-    </button>`;
-  }
-});
+    </button>
+*/
+
 
 // getElementsByClassName()
-// Returns an HTMLCollection
+// Returns an HTMLCollection that can't go through forEach, to do that you need to turn into array
 
-const listItems2 = document.getElementsByClassName('item');
+// Create a const listItems2 and use getElementsByClassName to get all elements with classname item 
 
-console.log(listItems2[2].innerText);
+// Console log the listItems2 index 2 innerText
 
-const listItemsArray = Array.from(listItems2);
+// Create a const listItemsArray that gets the listItems2 in array format using Array.from
 
-listItemsArray.forEach((item) => {
-  console.log(item.innerText);
-});
+// Now that listItemsArray is an array, go through it with forEach and console log item innerText
 
 // getElementsByTagName()
 
-const listItems3 = document.getElementsByTagName('li');
-console.log(listItems3[0].innerText);
+// Create a const listItem3 that gets the elements by tag use getElementsByTagName to get all li tag
+
+// Console log listItem3 index 0 innerText
+
