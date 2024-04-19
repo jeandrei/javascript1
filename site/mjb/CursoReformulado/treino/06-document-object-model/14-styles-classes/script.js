79f3d031cd2bda0e008fb1ad04396eedf0b395f2
jeandrei <jeandreiwalter@gmail.com>
2024-04-19 01:02:08 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/06-document-object-model/14-styles-classes/script.js
+++ site/mjb/CursoReformulado/treino/06-document-object-model/14-styles-classes/script.js
@@ -1,35 +1,21 @@
-const text = document.querySelector('p');
-const itemList = document.querySelector('.item-list');
-const items = itemList.querySelectorAll('li');
+/*Create a const text and select the p tag */
 
-function run() {
-  // className - Gets a string of all classes
-  console.log(itemList.className);
-  // Changing the classes with className
-  text.className = 'card dark';
+/*Create a const itemList and select all itens of class item-list */
 
-  // classList - Array of classes, which also has methods to add, remove, toggle and replace
-  console.log(itemList.classList);
+/*Create a const item and select all itemList li */
 
-  // We can loop through the classes
-  itemList.classList.forEach((c) => console.log(c));
+/*
+Create a function run
+Create a event to run the function as the button is clicked
+Inside console log itemList.className
+Apply the class card dark to the text className this way the actual classes will be replaced
+console log itemList.classList 
+as bonus using arrow function and forEach loop through the itemList.classList and console log the classe
+Use classList and add the class dark to the text the existing classes will not be removed
+Now using classList remove the text class card
+The same use toggle with the class hidden
+Now replace the class card with the class dark
+change the itemList lineHeight to 3 it is a css style 
+Using items forEach change the style color to red and if the index iqual to 2 change the color to blue 
+*/
 
-  // Add, remove, toggle, replace
-  text.classList.add('dark');
-  text.classList.remove('card');
-  text.classList.toggle('hidden');
-  text.classList.replace('card', 'dark');
-
-  // style property - Add styles to elements
-  itemList.style.lineHeight = '3';
-
-  items.forEach((item, index) => {
-    item.style.color = 'red';
-
-    if (index === 2) {
-      item.style.color = 'blue';
-    }
-  });
-}
-
-document.querySelector('button').onclick = run;
---
+++ site/mjb/CursoReformulado/treino/06-document-object-model/14-styles-classes/todo.js
@@ -0,0 +1,21 @@
+/*Create a const text and select the p tag */
+
+/*Create a const itemList and select all itens of class item-list */
+
+/*Create a const item and select all itemList li */
+
+/*
+Create a function run
+Create a event to run the function as the button is clicked
+Inside console log itemList.className
+Apply the class card dark to the text className this way the actual classes will be replaced
+console log itemList.classList 
+as bonus using arrow function and forEach loop through the itemList.classList and console log the classe
+Use classList and add the class dark to the text the existing classes will not be removed
+Now using classList remove the text class card
+The same use toggle with the class hidden
+Now replace the class card with the class dark
+change the itemList lineHeight to 3 it is a css style 
+Using items forEach change the style color to red and if the index iqual to 2 change the color to blue 
+*/
+
