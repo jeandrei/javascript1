a18336a05f160e635f8043edfe3f8a1f780a5385
jeandrei <jeandreiwalter@gmail.com>
2024-04-22 00:59:15 +0000

    atualzacao

---
+++ site/mjb/CursoReformulado/treino/07-events/02-mouse-events/shopping-list/resolved.js
@@ -0,0 +1,41 @@
+/* Seção 08 Aula 85  */
+const logo = document.querySelector('img');
+
+const onClick = () => console.log('click event');
+
+const onRightClick = () => console.log('right click event');
+const onMouseDown = () => console.log('mouse down event');
+const onMouseUp = () => console.log('mouse up event');
+const onMouseWheel = () => console.log('mouse wheel event');
+const onMouseOver = () => console.log('mouse over event');
+const onMouseOut = () => console.log('mouse out event');
+const onDragStart = () => console.log('drag start event');
+const onDrag = () => console.log('drag event');
+const onDragEnd = () => console.log('drag end event');
+
+// Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+// mousedown when click and hold
+logo.addEventListener('mousedown', onMouseDown);
+// mouseup when click hold and release the mouse button
+logo.addEventListener('mouseup', onMouseUp);
+// wheel the wheel of the mouse
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
+
+
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+};
