31b6ba79adc597357674b4447deb5a8bfdd8b2ec
jeandrei <jeandreiwalter@gmail.com>
2024-05-03 00:33:24 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/07-events/09-event-delegation/shopping-list/todo.js
+++ site/mjb/CursoReformulado/treino/07-events/09-event-delegation/shopping-list/todo.js
@@ -14,25 +14,3 @@ Then remove it
 As a bonus create another list addEventListener on mouse over check for the tagName if is LI
 And if so change the e target style color to red
 */
-const listItems = document.querySelectorAll('li');
-const list = document.querySelector('ul');
-
-// Add an event listener on all items
-// listItems.forEach((item) => {
-//   item.addEventListener('click', (e) => {
-//     e.target.remove();
-//   });
-// });
-
-// Add a single event listener on the parent (Event Delegation)
-list.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.remove();
-  }
-});
-
-list.addEventListener('mouseover', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.style.color = 'red';
-  }
-});
