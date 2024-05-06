91ab02433a0c9d51725a6d5184aa3eca075ae368
jeandrei <jeandreiwalter@gmail.com>
2024-05-06 00:23:02 +0000

    atualizacao

---
+++ site/mjb/CursoReformulado/treino/08-shopping-list-project/02-remove-clear-items/shopping-list/todo.js
@@ -0,0 +1,15 @@
+/**
+ * Create a itemList addEventListener click and call a function removeItem that we will create next
+ * Create a function removeItem getting the e object event
+ * To test what you are getting just console.log e.target
+ * Another test if you just make e.target.remove() and click the x button it will remove the button and not the item
+ * Now console e.target.parentElement and see that when you click at the x button what you get is the button because button is the parent of the icon
+ * Now if you console e.target.parentElement.classList you'll see all the classes of the item you clicked and when click in the x button remove-item is one of the classes
+ * To filter we can check if the parent of the item we clicked has this class remove-item
+ * so first check if the e.target.parentElement.classList.contains('remove-item') if so procede with the e.target.parentElement.parentElement.remove()
+ * Now create a const clearBtn and get the element id clear
+ * Create a clearBtn addEventListener click and call a function called clearItems that we will create next
+ * Now create a function callse clearItems
+ * And while listItem.firstChild remove the itemList.removeChild(itemList.firstChild)
+ * 
+ */
