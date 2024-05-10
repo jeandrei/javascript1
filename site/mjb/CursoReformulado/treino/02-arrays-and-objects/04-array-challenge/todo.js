61ce7cb2cf209dc53349499bd4a4344487ca1890
jeandrei <jeandreiwalter@gmail.com>
2024-05-10 15:49:25 +0000

    atualizacao

---
+++ site/mjb/CursoReformulado/treino/02-arrays-and-objects/04-array-challenge/todo.js
@@ -0,0 +1,21 @@
+// Challenge 1
+//Expected result [6, 5, 4, 3, 2, 1, 0];
+
+const arr = [1, 2, 3, 4, 5]
+// reverse the array
+// add zero to the end of the array
+// add 6 to the beginning
+
+console.log(arr)
+
+// Challenge 2
+
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [5, 6, 7, 8, 9, 10]
+//concatenate both arrays together, 5 can't repeat
+
+// Solution 1 slice first array till 4 and then cancat
+
+// Solution 2 use the pread opperator and then splice the last object
+
+console.log(arr4)
---
+++ site/mjb/CursoReformulado/treino/02-arrays-and-objects/05-object-literals/todo.js
@@ -0,0 +1,40 @@
+let x
+
+// Creating an object
+/*
+person
+name: John Doe
+age: 30
+isAdmin: true
+address
+  street: 123 Main st 
+  city: Boston
+  state: MA
+hobbies: music, sports
+*/
+
+// Accessing object properties
+// Dot notation access person name
+//x = person name
+
+// Bracket notation
+//x = person age
+
+//Dot notation
+//x = person adress state
+//x = person hobbies index zero
+
+// Updating properties
+// update person name to Jane Doe
+// update person isAdmin to false
+
+// Deleting properties
+// delete properties person age
+
+// Create new properties
+// create a propreri hasChildren and set it to true
+
+// Add functions person.greet that console Hello, my name is ${this.name}
+// call the function
+
+console.log(x)
