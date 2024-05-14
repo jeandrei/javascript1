bf6491a4c839b48f44304aff7002889854d513d6
jeandrei <jeandreiwalter@gmail.com>
2024-05-14 12:32:17 +0000

    atualizacao

---
+++ site/mjb/CursoReformulado/treino/03-functions-scope/07-arrow-functions/todo.js
@@ -0,0 +1,20 @@
+// Arrow function syntax
+// create a Function Expression arrow function add with two parameters a and b and return the sum of the values
+
+// Implicit Return when you have just one line of code
+/* create a const arrow function subtract as function that return a - b in just one line of code*/
+
+// Can leave off () with a single param
+/*Create an arrow function double that multiply a * 2
+as it has just one parameter you can leave off () */
+
+// Returning an object
+/*Create a const createObj arrow function that returns an object with name Brad to create an object before the {} you need to have () */
+
+/*Using arrow function with array
+create a const array numbers with 1 through five
+*/
+
+// create a forEach to show all values inside array numbers
+
+// now do the same but using Arrow function
---
+++ site/mjb/CursoReformulado/treino/03-functions-scope/08-iife/todo.js
@@ -0,0 +1,8 @@
+// ()(); IFFE Syntax (Has it's own scope and runs right away)
+/*Create a IFFE that has a cons user with John, console log user and then inside create another consg arrow function that console log Hello from the IIFE */
+
+// Params
+/*Create a IIFE that has a parameter name and then console log Hello name*/
+
+// Named IIFE (Can only be called recursively)
+/*create a IIFE hello that console log Hello */
