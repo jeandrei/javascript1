23b3fb9d88decca4ec41f2a82df9979fd52b8280
jeandrei <jeandreiwalter@gmail.com>
2024-05-29 00:05:35 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/09-asynchronous-javascript/09-promise-chaining/script.js
+++ site/mjb/CursoReformulado/treino/09-asynchronous-javascript/09-promise-chaining/script.js
@@ -1,29 +1,47 @@
+
+/**
+ * Create const promise with new promise
+ * then create a setTimeout with 1000ms
+ * inside the setTimeout create a const error set to true
+ * then check if no error resolve with a object name: 'John', age: 30
+ * and reject with 'Error: Something went wrong'
+ * 
+ * Then chain the promise clg user and return username
+ * clg name returning name length
+ * clg nameLength
+ * catch clg error and returning 123
+ * and at the end clg 'This will run no matter what', x
+ */
+
+
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let error = true;
-
-    if (!error) {
-      resolve({ name: 'John', age: 30 });
+    const error = true
+    if(!error){
+      resolve({name: 'John', age: 30})
     } else {
-      reject('Error: Something went wrong');
+      reject('Something went wrong...')
     }
-  }, 1000);
-});
+  },1000)
+})
+
 
 promise
-  .then((user) => {
-    console.log(user);
-    return user.name;
-  })
-  .then((name) => {
-    console.log(name);
-    return name.length;
-  })
-  .then((nameLength) => {
-    console.log(nameLength);
-  })
-  .catch((error) => {
-    console.log(error);
-    return 123;
-  })
-  .then((x) => console.log('This will run no matter what', x));
+.then((user) => {
+  console.log(user)
+  return user.name
+})
+.then((name) => {
+  console.log(name)
+  return name.length
+})
+.then((nameLength) => {
+  console.log(nameLength)
+})
+.catch((err) =>{
+  console.log(err);
+  return 123;
+})
+.then((x) => {
+  console.log('This will run no metter what, ' + x);
+})
---
+++ site/mjb/CursoReformulado/treino/09-asynchronous-javascript/09-promise-chaining/todo.js
@@ -0,0 +1,14 @@
+
+/**
+ * Create const promise with new promise
+ * then create a setTimeout with 1000ms
+ * inside the setTimeout create a const error set to true
+ * then check if no error resolve with a object name: 'John', age: 30
+ * and reject with 'Error: Something went wrong'
+ * 
+ * Then chain the promise clg user and return username
+ * clg name returning name length
+ * clg nameLength
+ * catch clg error and returning 123
+ * and at the end clg 'This will run no matter what', x
+ */
