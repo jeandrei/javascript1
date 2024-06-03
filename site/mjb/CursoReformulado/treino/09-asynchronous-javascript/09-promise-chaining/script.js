1b0079e0d5cd69b884336ddac7c4f17aa6ad0db9
jeandrei <jeandreiwalter@gmail.com>
2024-06-03 01:25:53 +0000

    aualizacao

--- site/mjb/CursoReformulado/treino/09-asynchronous-javascript/09-promise-chaining/script.js
+++ site/mjb/CursoReformulado/treino/09-asynchronous-javascript/09-promise-chaining/script.js
@@ -12,36 +12,3 @@
  * catch clg error and returning 123
  * and at the end clg 'This will run no matter what', x
  */
-
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const error = true
-    if(!error){
-      resolve({name: 'John', age: 30})
-    } else {
-      reject('Something went wrong...')
-    }
-  },1000)
-})
-
-
-promise
-.then((user) => {
-  console.log(user)
-  return user.name
-})
-.then((name) => {
-  console.log(name)
-  return name.length
-})
-.then((nameLength) => {
-  console.log(nameLength)
-})
-.catch((err) =>{
-  console.log(err);
-  return 123;
-})
-.then((x) => {
-  console.log('This will run no metter what, ' + x);
-})
--- site/mjb/CursoReformulado/treino/09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ site/mjb/CursoReformulado/treino/09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -1,36 +1,28 @@
-function getData(endpoint) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+/**
+ * Refector this code using promise
+ */
+function getData(endpoint, cb) {
+  const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', endpoint);
+  xhr.open('GET', endpoint);
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject('Something went wrong');
-        }
-      }
-    };
+  xhr.onreadystatechange = function () {
+    if ((this.readyState === 4) & (this.status === 200)) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
 
-    setTimeout(() => {
-      xhr.send();
-    }, Math.floor(Math.random() * 3000) + 1000);
-  });
+  setTimeout(() => {
+    xhr.send();
+  }, Math.floor(Math.random() * 3000) + 1000);
 }
 
-// Whatever we return from a .then() is passed into the next .then() callback function args
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+getData('./movies.json', (data) => {
+  console.log(data);
+  getData('./actors.json', (data) => {
+    console.log(data);
+    getData('./directors.json', (data) => {
+      console.log(data);
+    });
+  });
+});
