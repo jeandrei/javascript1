802d2b055cd5eb2c5fd083eaea0ce5d20f224e61
jeandrei <jeandreiwalter@gmail.com>
2024-08-06 15:39:39 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/Web API/Canvas/ClockMiniProject/animated-clock-1/animated-clock/script.js
+++ site/mjb/CursoReformulado/treino/Web API/Canvas/ClockMiniProject/animated-clock-1/animated-clock/script.js
@@ -44,12 +44,56 @@ function clock(){
       ctx.moveTo(117,0);
       ctx.lineTo(120,0);
       ctx.stroke();
-    } //@todo 16:19 
+    }  
     ctx.rotate(Math.PI / 30);  
   }  
   ctx.restore();
 
+  // Get current time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+  //console.log(`${hr}:${min}:${sec}`);
+
+  // Draw hour hand
+  ctx.save();
+  ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 14;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw min hand
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw sec hand
+  ctx.save();
+  ctx.rotate((sec * Math.PI / 30));
+  ctx.strokeStyle = '#FF7F50';
+  ctx.fillStyle = '#FF7F50';
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(100, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
   ctx.restore(); //restore default state
+  requestAnimationFrame(clock);
 }
 
-clock();
+requestAnimationFrame(clock);
