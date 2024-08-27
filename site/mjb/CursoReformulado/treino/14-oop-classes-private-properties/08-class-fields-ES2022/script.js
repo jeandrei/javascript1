24392428a604d23a1cb341567440e65d7bcf09a0
jeandrei <jeandreiwalter@gmail.com>
2024-08-27 00:47:20 +0000

    atualizacao

--- site/mjb/CursoReformulado/treino/14-oop-classes-private-properties/08-class-fields-ES2022/script.js
+++ site/mjb/CursoReformulado/treino/14-oop-classes-private-properties/08-class-fields-ES2022/script.js
@@ -1,50 +1,25 @@
-class Wallet {
-  #balance = 0;
-  #transactions = [];
+/**
+ * Recreate the privious exercise but this time use the ES2022 # character to set private properties
+ * 
+ * create a class Wallet with a private balance set to zero and an private array transactions set to [] all
+ * 
+ * now first let's create a private method processDeposit(amount)
+ * simple console log Depositing value and push to the transection array an object type: 'deposit' and the amount
+ * 
+ * now create a public method deposit 
+ * processDeposit and then set the private balanc to balance + amount
+ * 
+ * now create a private processWidthdraw that will do the same as processDeposit but it will 
+ * console log Withdrawing value and push to transaction type withdraw and the amount
+ * now create a public method withdraw(amount) that will processWithdraw and update balance - amount
+ * avoid withdraw more than the balance show a message in the console Not enough funds
+ * 
+ * then create a get balance and a get transactions
+ * 
+ * now create a new wallet
+ * deposit 300
+ * withdraw 50
+ * console.log(wallet.balance)
+ * console.log(wallet.transactions)
+ */
 
-  deposit(amount) {
-    this.#processDeposit(amount);
-    this.#balance += amount;
-  }
-
-  withdraw(amount) {
-    if (amount > this.#balance) {
-      console.log('Not enough funds');
-      return;
-    }
-
-    this.#processWithdraw(amount);
-    this.#balance -= amount;
-  }
-
-  #processDeposit(amount) {
-    console.log(`Depositing ${amount}`);
-
-    this.#transactions.push({
-      type: 'deposit',
-      amount,
-    });
-  }
-
-  #processWithdraw(amount) {
-    console.log(`Withdrawing ${amount}`);
-
-    this.#transactions.push({
-      type: 'withdraw',
-      amount,
-    });
-  }
-
-  get balance() {
-    return this.#balance;
-  }
-
-  get transactions() {
-    return this.#transactions;
-  }
-}
-
-const wallet = new Wallet();
-wallet.deposit(500);
-wallet.withdraw(100);
-console.log(wallet.balance);
---
+++ site/mjb/CursoReformulado/treino/14-oop-classes-private-properties/08-class-fields-ES2022/todo.js
@@ -0,0 +1,25 @@
+/**
+ * Recreate the privious exercise but this time use the ES2022 # character to set private properties
+ * 
+ * create a class Wallet with a private balance set to zero and an private array transactions set to [] all
+ * 
+ * now first let's create a private method processDeposit(amount)
+ * simple console log Depositing value and push to the transection array an object type: 'deposit' and the amount
+ * 
+ * now create a public method deposit 
+ * processDeposit and then set the private balanc to balance + amount
+ * 
+ * now create a private processWidthdraw that will do the same as processDeposit but it will 
+ * console log Withdrawing value and push to transaction type withdraw and the amount
+ * now create a public method withdraw(amount) that will processWithdraw and update balance - amount
+ * avoid withdraw more than the balance show a message in the console Not enough funds
+ * 
+ * then create a get balance and a get transactions
+ * 
+ * now create a new wallet
+ * deposit 300
+ * withdraw 50
+ * console.log(wallet.balance)
+ * console.log(wallet.transactions)
+ */
+
