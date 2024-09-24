dbb7b8afef0f48fad174a8bc714f38995a61a750
jeandrei <jeandreiwalter@gmail.com>
2024-09-24 00:30:09 +0000

    update

--- site/mjb/CursoReformulado/treino/16-modules-and-tooling/04-es-modules/app.js
+++ site/mjb/CursoReformulado/treino/16-modules-and-tooling/04-es-modules/app.js
@@ -1,8 +1,20 @@
-import { capitalizeWords, makeMoney } from './modules/utils.js';
-import Person from './modules/Person.js';
-
-console.log(capitalizeWords('hello world'));
-console.log(makeMoney(100));
-
-const person = new Person('Mark', 29);
-person.greet();
+/**
+ * In modules folder we have utils.js and a class Person inside Person.js
+ * In es modules we export and import it differently
+ * to export we use the sintax export default module if one module
+ * export {module1, module2, module3} if more than one mudule
+ * to import we don't use require
+ * we use import from
+ * like 
+ * import module from "path" when one module
+ * import {module1, module2, module3} from path when more than one module
+ * and so at app.js import capitalizeWords, makeMoney 
+ * then import Person
+ * then 
+ * console.log(capitalizeWords('hello world'));
+ * console.log(makeMoney(100));
+ * const person = new Person('Mark', 29);
+ * person.greet();
+ * 
+ * 
+ */
---
+++ site/mjb/CursoReformulado/treino/16-modules-and-tooling/04-es-modules/resolved.js
@@ -0,0 +1,9 @@
+/* Seção 17 Aula 208  */
+import { capitalizeWords, makeMoney } from './modules/utils.js';
+import Person from './modules/Person.js';
+
+console.log(capitalizeWords('hello world'));
+console.log(makeMoney(100));
+
+const person = new Person('Mark', 29);
+person.greet();
